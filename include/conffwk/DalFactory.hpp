8d620826d39576cd529dc78c6c04639ebece523a
Alessandro Thea <alessandro.thea@cern.ch>
2025-05-15 06:48:30 +0200

    Typo fixed

--- include/conffwk/DalFactory.hpp
+++ include/conffwk/DalFactory.hpp
@@ -16,7 +16,7 @@ namespace dunedaq {
 
 ERS_DECLARE_ISSUE(conffwk, DalPackageNameNotFound,
               "Failed to find the dal package name for class " << class << " in its schema path '" << schema_path << "'",
-              ((std::string)class))((std::string)schema_path))
+              ((std::string)class))((std::string)schema_path)
 
 ERS_DECLARE_ISSUE(conffwk, LoadDalFailed,
                 "Could not load dal shared library " << library,
