23e33922ab0a40c013714a4bbda04efb4044dbe2
Alessandro Thea <alessandro.thea@cern.ch>
2025-05-22 15:15:28 +0200

    Cleanup of factory methods

--- include/conffwk/details/DalFactory.hxx
+++ include/conffwk/details/DalFactory.hxx
@@ -4,25 +4,12 @@
 namespace dunedaq{
 namespace conffwk{
 
-/** register DAL object creator by class name*/
-template<class T>
-void
-DalFactory::register_dal_class(const std::string & name, const std::set<std::string>& algorithms)
-{
-  std::lock_guard<std::mutex> scoped_lock(m_class_mutex);
 
-  TLOG_DEBUG(1) << "register class " << name;
-
-  if (m_classes.emplace(name, DalFactoryFunctions(boost::compute::identity<T>(), algorithms)).second == false)
-    {
-      TLOG_DEBUG(0) << "class " << name << " was already registered";
-    }
-}
 
 /** register DAL object creator by class name*/
 template<class T>
 void
-DalFactory::register_dal_class_2g(const std::string & name)
+DalFactory::register_dal_class(const std::string & name)
 {
   std::lock_guard<std::mutex> scoped_lock(m_class_mutex);
 
